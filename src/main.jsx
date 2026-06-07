abd91bfd9f916719beea40cf33716b69333b4069
junchoi0521-cpu <junchoi0521@gmail.com>
2026-06-07 17:26:16 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -365,5 +365,3 @@ function StickyButton() { return <button className="sticky" type="button" onClic
 
 const path = window.location.pathname;
 createRoot(document.getElementById('root')).render(path === '/privacy' ? <PolicyPage type="privacy" /> : path === '/terms' ? <PolicyPage type="terms" /> : path === '/thanks' ? <ThanksPage /> : <App />);
-
-// redeploy trigger
